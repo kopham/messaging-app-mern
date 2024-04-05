4b9767e9035424f7d16c50397d5620379581e341
kopham <khoi.2.pham@ucdenver.edu>
2024-04-04 18:19:20 -0600

    first commit

---
+++ messaging-app-frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Sidebar from './components/Sidebar';
+import Chat from './components/Chat';
+function App() {
+  return (
+    <div className="App">
+      <div className="app__body">
+        <Sidebar />
+        <Chat />
+      </div> 
+    </div>
+  );
+}
+
+export default App;
