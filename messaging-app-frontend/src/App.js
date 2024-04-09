433f999a23b92f97cce96879172237c1d35b69c7
kopham <khoi.2.pham@ucdenver.edu>
2024-04-09 12:27:51 -0600

    new changes

--- messaging-app-frontend/src/App.js
+++ messaging-app-frontend/src/App.js
@@ -1,7 +1,30 @@
 import './App.css';
 import Sidebar from './components/Sidebar';
 import Chat from './components/Chat';
+import React, {useEffect, useState} from 'react'
+import Pusher from 'pusher-js'
+
 function App() {
+const[messages, setMessages] = useState([])
+
+useEffect(() => {
+  const pusher = new Pusher('67e04ac25de523ac99c0', {
+    cluster: 'us3'
+  });
+
+  const channel = pusher.subscribe('message');
+  channel.bind('inserted', (data) => {
+    setMessages([...messages, data])
+  });
+
+  return () => {
+    channel.unbind_all()
+    channel.unsubscribe()
+  }
+}, [messages])
+
+console.log(messages)
+
   return (
     <div className="App">
       <div className="app__body">
